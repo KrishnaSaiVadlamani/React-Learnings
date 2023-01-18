3e9262ec5c3c5ef8a8fe192a87dbed6dfc27b188
KrishnaSaiVadlamani <krishnasai.vadlamani@zemosolabs.com>
2023-01-18 17:28:08 +0530

    practice App using Virtual Dom

---
+++ practiceApp/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+  const [flag,setFlag] = useState(false);
+  // const value = false;
+  return (
+    <div className="App">
+       {/* Hello Krishna!!! */}
+      {flag?"Hello GentleMan":"Hello Mannnn"}
+      <br/>
+      <button onClick={
+        ()=>setFlag(!flag)
+        // {value=true;}
+      }>Click Me</button>
+
+    </div>
+  );
+}
+
+export default App;
